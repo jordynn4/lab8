a84fe2013d48f4c1f3d0330a235ad231c080cc15
Jordynn Bartolome <jordynnbartolome@gmail.com>
2019-02-28 10:08:42 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'kgNvPGDuKtpD4zZHJ46GoLjR6ApSgGd5';
+
+	// "map" refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.878973, -117.235925],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.878973, -117.235925]).addTo(map);
 }
